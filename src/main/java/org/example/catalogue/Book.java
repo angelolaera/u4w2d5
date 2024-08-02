0d8a9b09f73dfa9a2acd858e8d35bafefe24068e
angelolaera <angelo.laera.1995@gmail.com>
2024-08-02 14:15:05 +0200

    Book.java

--- src/main/java/org/example/catalogue/Book.java
+++ src/main/java/org/example/catalogue/Book.java
@@ -1,4 +1,28 @@
 package org.example.catalogue;
 
-public class Book {
+public class Book extends Catalog {
+    private String author;
+    private String genre;
+
+    public Book(String isbn, String title, int publicationYear, int pageCount, String author, String genre) {
+        super(isbn, title, publicationYear, pageCount);
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                "} " + super.toString();
+    }
 }
